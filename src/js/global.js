457282d2325cce36d153c2922a5f4acb509f2b0a
Nikita-Hunter <mochalov-hunter@mail.ru>
2021-11-05 17:48:04 +0300

    make dropdown js

--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,10 @@
-console.log('global');
+document.addEventListener('DOMContentLoaded', function () {
+    var itemHover = document.getElementById('openDropDodwn')
+    var dropDown = document.getElementById('dropDown')
+    itemHover.addEventListener('mouseenter', function (e) {
+        dropDown.classList.add('dropdown-show');
+    })
+    itemHover.addEventListener('mouseleave', function () {
+        dropDown.classList.remove('dropdown-show')
+    })
+})
