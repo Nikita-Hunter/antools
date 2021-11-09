6a97216fad9afa1cd55ab4a933ec2f125e557c15
Nikita-Hunter <mochalov-hunter@mail.ru>
2021-11-09 19:44:14 +0300

    make mobile desktop

--- src/js/global.js
+++ src/js/global.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function () {
         dropDown.classList.remove('dropdown-show')
     })
 });
+// document.onkeydown = function(event) {
+//     if (event.ctrlKey && (event.keyCode == 85) || (event.ctrlKey && event.shiftKey && (event.keyCode == 73)) || event.keyCode == 123) {
+//         return false
+//     }
+// };
+// document.oncontextmenu = function(){
+//     return false
+// };
+var  colNav = document.getElementById('colNav')
+var btnMenu = document.getElementById('btnMenu')
+btnMenu.addEventListener('click', function(){
+    colNav.classList.toggle('show-nav')
+});
